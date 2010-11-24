419eb9c57da9071c8e8c51ad2fb48b95307554da
Waseem Ahmad <cougar2149@gmail.com>
2010-11-24 09:59:07 +0530

    Restricted validation for albums for a while.

--- lib/joey/album.rb
+++ lib/joey/album.rb
@@ -21,15 +21,15 @@ module Joey
       errors << { :message => "location is neither string nor nil but is #{location.inspect}" } unless location.is_a?(String) || location.nil?
       errors << { :message => "from is not a Joey::User or Joey::Page and is #{from.inspect}" } unless from.is_a?(Joey::User) || from.is_a?(Joey::Page)
 
-      errors << { :message => "comments is not an array nor nil and is #{comments.inspect}" } unless comments.is_a?(Array) || comments.nil?
-      if comments.is_a?(Array) && !(comments.collect(&:class).uniq - [Joey::Comment]).empty?
-        errors << { :message => 'comments is not an array of Joey::Comment' }
-      end
+      #errors << { :message => "comments is not an array nor nil and is #{comments.inspect}" } unless comments.is_a?(Array) || comments.nil?
+      #if comments.is_a?(Array) && !(comments.collect(&:class).uniq - [Joey::Comment]).empty?
+        #errors << { :message => 'comments is not an array of Joey::Comment' }
+      #end
 
-      errors << { :message => "photos is not an array nor nil and is #{photos.inspect}" } unless photos.is_a?(Array) || photos.nil?
-      if photos.is_a?(Array) && !(photos.collect(&:class).uniq - [Joey::Photo]).empty?
-        errors << { :message => 'photos is not an array of Joey::Comment' }
-      end
+      #errors << { :message => "photos is not an array nor nil and is #{photos.inspect}" } unless photos.is_a?(Array) || photos.nil?
+      #if photos.is_a?(Array) && !(photos.collect(&:class).uniq - [Joey::Photo]).empty?
+        #errors << { :message => 'photos is not an array of Joey::Comment' }
+      #end
     end
 
     def valid?
